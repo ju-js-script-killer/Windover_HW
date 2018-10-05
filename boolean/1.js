2d5db7d5f65c6b9117ba2d7d8906066edbd9ca78
ju-js-script-killer <julesbronnikova@gmail.com>
2018-10-06 00:14:01 +0300

    d

--- boolean/1.js
+++ boolean/1.js
@@ -5,37 +5,36 @@ console.log('a = ' + a);
 console.log(a > 0);
 
 //task 2
-var a = Math.ceil(Math.random() * 100) % 2;
+var a = Math.ceil(Math.random() * 100);
 console.log('a = ' + a);
-console.log(a > 0);
+console.log(a % 2 > 0);
 
 //task 3
-var a = Math.ceil(Math.random() * 100) % 2;
+var a = Math.ceil(Math.random() * 100);
 console.log('a = ' + a);
-console.log(a <= 0);
+console.log(a % 2 == 0);
 
 //task 4
 var a = Math.ceil(Math.random() * 10);
 var b = Math.ceil(Math.random() * 10);
 console.log('a = ' + a);
-console.log(a > 2);
 console.log('b = ' + b);
-console.log(b <= 3);
+console.log(a > 2 && b <= 3);
 
-//task 5
+//task 5 Переделать
 var a = Math.ceil(Math.random() * 10) -7;
 var b = Math.ceil(Math.random() * 10) -7;
 console.log('a = ' + a);
 console.log(a >= 0);
 console.log('b = ' + b);
-console.log(b < -3);
+console.log(b < -2);
 
-//task 6
+//task 6 Переделать
 var a = Math.ceil(Math.random() * 10) -5;
 var b = Math.ceil(Math.random() * 10) -5;
 var с = Math.ceil(Math.random() * 10) -5;
 console.log('a = ' + a + ";"+" "+'b = ' + b + ";"+" "+ 'с = ' + с);
-console.log(b > a);
+console.log(&& ;
 console.log(b < c);
 
 var a = Math.ceil(Math.random() * 10) -5;
--- integer/1.js
+++ integer/1.js
@@ -7,12 +7,12 @@ console.log(Math.floor(l / 100));
 // task 2
 var m = Math.ceil(Math.random() * 1000);
 console.log('m = ' + m);
-console.log(Math.round(m / 1000));
+console.log(Math.floor(m / 1000));
 
 // task 3
 var byte = Math.ceil(Math.random() * 100000);
 console.log('byte = ' + byte);
-console.log(Math.round(byte / 1024));
+console.log(Math.floor(byte / 1024));
 
 // task 4
 var a = Math.ceil(Math.random() * 1000);
@@ -26,10 +26,10 @@ var a = Math.ceil(Math.random() * 1000);
 var b = Math.ceil(Math.random() * 100);
 console.log('a = ' + a);
 console.log('b = ' + b);
-console.log(Math.floor(a % b));
+console.log(a % b);
 
 // task 6
 var b = Math.ceil(Math.random() * 100);
 console.log('b = ' + b);
 console.log(Math.floor(b / 10));
-console.log(Math.floor(b % 10));
+console.log(b % 10);
