dbe36eaae838c11e56a882bc2d37b883d166078f
ju-js-script-killer <julesbronnikova@gmail.com>
2018-10-16 19:01:14 +0300

    while

--- If/For.js
+++ If/For.js
@@ -118,3 +118,69 @@ console.log("1 kg of candy costs",sweet);
    for (var i = 12; i <= 20; i+=2) {
     console.log (i/10 + " " + "kg cost" + " " + i/10*sweet);
   }
+
+  /* Task 7. Cycle for. Даны два целых числа a и b (a < b).
+  Найти сумму всех целых чисел от a до b включительно. */
+  var a = Math.ceil (Math.random() * 10);
+  console.log ("a = ", a);
+  var b = Math.ceil (Math.random() * 20);
+  console.log ("b = ", b);
+  var sum = 0;
+    for (var i = a; i <= b; i++) {
+      sum  +=i;
+    }
+  console.log (sum);
+
+  /* Task 8. Cycle for. Даны два целых числа a и b (a < b).
+  Найти произведение всех целых чисел от a до b включительно. */
+  var a = Math.ceil (Math.random() * 10);
+  console.log ("a = ", a);
+  var b = Math.ceil (Math.random() * 20);
+  console.log ("b = ", b);
+  var sum = 1;
+    for (var i = a; i <= b; i++) {
+      sum *= i;
+    }
+  console.log (sum);
+
+  /* Task 9. Cycle for. Даны два целых числа a и b (a < b).
+  Найти сумму квадратов всех целых чисел от a до b включительно. */
+var a = 2;
+var b = 5;
+var sum = 0;
+  for ( var i = a; i <= b; i++) {
+    sum = sum + i * i;
+  }
+console.log(sum);
+
+/* Task 10. Cycle for. Дано целое число N ( N > 0).
+Найти сумму 1 + 1/2 + 1/3 + ...+ 1/N (вещественное число). */
+var N = 6;
+var sum = 0;
+  for (var i = 1; i <= N; i++) {
+    sum = sum + 1/i;
+  }
+console.log(sum);
+
+/* Task 11. Cycle for. Дано целое число N ( N > 0).
+Найти сумму N*N + (N+1)квадрат + (N+2)квадрат + ...+ (2*N)квадрат (целое число). */
+var N = 4;
+var sum = 0;
+var sum_final;
+for (var i = 0; i <= N; i++) {
+  sum = sum + Math.pow(N + i, 2);
+}
+
+sum_final = sum + Math.pow(2 * N, 2);
+console.log(sum_final);
+
+// Task 11.1 ??? Разобрать
+var N = 4;
+var sum = 0;
+for (var i = 0; i <= N; i++) {
+  sum = sum + Math.pow(N + i, 2);
+  if (i == N) {
+    Math.pow(N + i, 2)*2;
+  }
+}
+console.log(sum);
