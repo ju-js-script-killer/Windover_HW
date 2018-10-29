cddeed26672c53e6fe4e83ca72e4e8b045579602
ju-js-script-killer <38808240+ju-js-script-killer@users.noreply.github.com>
2018-10-29 17:48:48 +0300

    new task

--- Function/fun examples.js
+++ Function/fun examples.js
@@ -1,4 +1,5 @@
-/*Функция это отрывок кода который может быть вызван из другого кода или вызван собой, или это variable которая ссылается на функцию.
+/*Функция это отрывок кода который может быть вызван из другого кода или вызван собой,
+или это variable которая ссылается на функцию.
 Когда функция вызвана, аргументы переданы в функцию как вход,
 и функция может необязательно вернуть вывод. Функция в JavaScript
  это object.
@@ -111,3 +112,36 @@ function foo () {
 }
 
 setInterval (foo, 1000);
+
+/*Example 7.*/
+var a = {"qwe": 10, "asd": 20};
+var b = [ 10, 20 ];
+
+console.log(a ["qwe"], a.qwe, a["asd"], a.asd);
+console.log(b[0], b ["0"], b[1], b ["1"]);
+
+/*Example 8.*/
+var a = {
+  name: "Jules",
+  age: 29,
+  sayHello: function () {
+    console.log ("Hello! My name is " + this.name + ". I am "  + this.age + " years old. " );
+  } /* this.name вызывает свойство из объекта*/
+};
+
+a.sayHello();
+
+/*Example 8.1*/
+function Human(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayHello = function () {
+      alert("Hi from " + this.name + ". Who is " + this.age + " years old.");
+  };
+}
+
+var a = new Human ("Jules", 20);
+var b = new Human ("Jusy", 30);
+
+a.sayHello ();
+b.sayHello ();
