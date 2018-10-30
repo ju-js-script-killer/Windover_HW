eeef1ab805ef9133b4a135d3c33cfb6bd149c01c
ju-js-script-killer <38808240+ju-js-script-killer@users.noreply.github.com>
2018-10-30 18:52:39 +0300

    new

---
+++ Theoretical  JS/Serching_elements/SearchingElements.js
@@ -0,0 +1,9 @@
+/*Есть 6 соновных методов поиска элементов DOM*/
+
+/*Метод */                    /*Ищет по...*/      /*ищет внутри элемента */
+getElementById                 id                     -
+getElementsByName              name                   -
+getElementsByTagName           тег или '*'            yes
+getElementsByClassName         классу                 yes
+querySelector                  CSS-селектор           yes
+querySelectorAll               CSS-селектор           yes
